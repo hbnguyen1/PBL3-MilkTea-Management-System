646ed8f272a19f98c654eef405870aaed5df5ee0
nguyenxuanngoctran2006-cloud <nguyenxuanngoctran2006@gmail.com>
2026-03-25 08:53:18 +0700

    kkk

--- PBL3/Core/Logger.cs
+++ PBL3/Core/Logger.cs
@@ -5,7 +5,7 @@ namespace PBL3.Core
 {
     internal class Logger
     {
-        private static string logFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"system_log.txt");
+        private static string logFile = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName, "system_log.txt");
 
         public static void Log(string message)
         {
