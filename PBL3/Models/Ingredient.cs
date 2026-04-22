2bc0e9e24a56fe4ddd084d30e45b0ef4c71e83f1
tamfpt1234-hue <tamfpt1234@gmail.com>
2026-04-22 23:30:28 +0700

    update docker

--- PBL3/Models/Ingredient.cs
+++ PBL3/Models/Ingredient.cs
@@ -9,7 +9,7 @@ namespace PBL3.Models
         public int igID { get; set; }
         public required string igName { get; set; } = string.Empty;
         public required int igCount { get; set; } = 0;
-        public required int price { get; set; } = 0;
+        public required double price { get; set; } = 0;
         public required string unit { get; set; } = string.Empty;
     }
 }
