53d379fafa40dcb7f0335f47bda143515834d845
Hoang Bao Nguyen <117627302+hbnguyen1@users.noreply.github.com>
2026-05-16 13:09:47 +0700

    merge fix performance to develop
    * improve performance by using Dependency Injection
    
    * fix: resolve N+1 queries, thread-safety, and EF Core mapping bugs
    
    * add recipe.cs

---
+++ PBL3/Models/Recipe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PBL3.Models
+{
+    [Table("RECIPE")]
+    public class Recipe
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int recipeID { get; set; }
+
+        public string size { get; set; } = string.Empty;
+
+        public int itemID { get; set; }
+
+        public int ingredientID { get; set; }
+
+        public int quantityNeeded { get; set; }
+
+        public string unitUsed { get; set; } = string.Empty;
+
+        public virtual Item Item { get; set; } = null!;
+
+        public virtual Ingredient Ingredient { get; set; } = null!;
+    }
+}
